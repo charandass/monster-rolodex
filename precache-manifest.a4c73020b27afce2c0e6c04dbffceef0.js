2b3898a2b93b0328bd9d451437854683bc9572b1
charandass <charan1631@gmail.com>
2020-03-08 12:48:12 +0530

    Updates

---
+++ precache-manifest.a4c73020b27afce2c0e6c04dbffceef0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78de0cc67ebf66955df8f994f13f8eb7",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "07dc6c0ecbd280d982a1",
+    "url": "/monster-rolodex/static/css/main.84f9aff4.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/monster-rolodex/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07dc6c0ecbd280d982a1",
+    "url": "/monster-rolodex/static/js/main.43330bd6.chunk.js"
+  },
+  {
+    "revision": "85cb2c00df0e1ad7d48d",
+    "url": "/monster-rolodex/static/js/runtime-main.3e508d1c.js"
+  }
+]);
